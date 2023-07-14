3e45c68dde45b65ddc2fccae1909a0e5310815a7
skp544 <shubhamkumarprajapati544@gmail.com>
2023-07-15 00:51:20 +0530

    api test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 const App = () => {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("error to fetch api");
+        console.log(err);
+      });
+  };
+
   return <div>App</div>;
 };
 
